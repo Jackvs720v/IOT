fef251fe50b5d8ffbc5daf611f4db00868770641
Jackvs720v <105511756+Jackvs720v@users.noreply.github.com>
2025-02-10 22:17:56 -0300

    commit final do dia

---
+++ aula2/LandingPage.h
@@ -0,0 +1,6 @@
+#ifndef TESTE123 //Verifica se ja leu a biblioteca 
+#define LANDING_H
+
+#const char index_html = R"rawliteral(
+colar html
+)";
--- aula2/main.ino
+++ aula2/main.ino
@@ -8,10 +8,35 @@
 WebServer server(80) 
 
 void setup(){
+    Serial.begin(9600);
+
     Wifi.mode(WIFI_OFF);
     delay(1000);
     Wifi.mode(WIFI_AP);
     delay(1000);
 
-    
+    IPAddress staticIP(192,168,10,17);
+    IPAddress gateway(192,168,10,1);
+    IPAddress subnet(255,255,255,0);
+
+    Wifi.softAPConfig(staticIP, gateway, subnet);
+    Wifi.softAP(WIFI_NAME,WIFI_PASS);
+
+    Serial.print("IP Configurado: ");
+    Serial.println(Wifi.softAPIP());
+
+    /*WEB SERVER: */
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { //Quando acessar a raiz da url, 
+        request -> send_P(200, "text/html", index_html);
+    });
+
+     server.onNotFound(notFound);
+    });
+    server.begin();
+
+
+}
+
+void notFound(AsyncWebServerRequest *request1){
+    request -> send_P(404, "text/html", notFound_html)
 }
