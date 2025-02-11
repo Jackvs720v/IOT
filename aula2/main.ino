48a59babd84112472447150f2a5b1e9b0bdabc8e
Jackvs720v <105511756+Jackvs720v@users.noreply.github.com>
2025-02-10 21:53:17 -0300

    Update main.ino

--- aula2/main.ino
+++ aula2/main.ino
@@ -5,3 +5,13 @@
 #define WIFI_NAME "Esp32WebServer" //Alterar para não ter problema na proxima aula
 #define WIFI_PASS "senaimange" //Alterar para não ter problema na proxima aula
 
+WebServer server(80) 
+
+void setup(){
+    Wifi.mode(WIFI_OFF);
+    delay(1000);
+    Wifi.mode(WIFI_AP);
+    delay(1000);
+
+    
+}
