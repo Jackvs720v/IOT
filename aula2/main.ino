df04039091b20bd11977757fe35ebe0e77ee7972
Jackvs720v <105511756+Jackvs720v@users.noreply.github.com>
2025-02-10 21:48:22 -0300

    primeiro Commit

---
+++ aula2/main.ino
@@ -0,0 +1,7 @@
+#include <Wifi.h>
+#include <WebServer.h>
+#include "LandingPage.h" //Faz como um axios point, ela é um wifi local
+
+#define WIFI_NAME "Esp32WebServer" //Alterar para não ter problema na proxima aula
+#define WIFI_PASS "senaimange" //Alterar para não ter problema na proxima aula
+
