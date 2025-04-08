c9b8ac9ceb046f221c24988c1377c8b0d2764004
Jackvs720v <105511756+Jackvs720v@users.noreply.github.com>
2025-04-07 22:19:49 -0300

    aula domotica

---
+++ correcaoProfDomotica/domotica/src/services/cdnServices.ts
@@ -0,0 +1,7 @@
+
+import { getCdnAxios } from "./services.config";
+
+export const getDevices = ()=>{
+    getCdnAxios().get("?content_type=device");
+    
+}
---
+++ correcaoProfDomotica/domotica/src/services/services.config.ts
@@ -0,0 +1,33 @@
+import axios, {AxiosHeaders, type AxiosResponse} from "axios";
+export const BASE_URL = import.meta.env.CONTENTFUL_BASE_ENDPOINT;
+
+export const getCdnAxios = (headers?: AxiosHeaders) =>{
+    const token = import.meta.env.CONTENTFUL_CDN_TOKEN;
+    getAxios("cdn",token,headers);
+}
+
+export const getApiAxios = (headers?: AxiosHeaders)=> {
+    const token = import.meta.env.CONTENTFUL_API_TOKEN;
+    return getAxios("api",token,headers);
+
+export const getAxios = (target: 'cdn'|'api', token:String,
+     headers?: AxiosHeaders) => {
+        if(headers===null) headers = new AxiosHeaders();
+    headers.set('Authorization', `Bearer ${token}`)
+    headers.set('Content-Type', `aplication/json`)
+
+    const createdAxios = axios.create({
+        baseURL: `https://${target}${BASE_URL}`,
+        timeout: 4000,
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": 'aplication/json'
+    }
+    });
+    createdAxios.interceptors.response.use(getAxiosResponse);
+    return createdAxios;
+}
+
+const getAxiosResponse = (response:AxiosResponse)=> {
+    return response.data;
+}
